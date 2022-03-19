b3ec1329a2bc31345359e8de1696da530ea85a87
NoahLaroi <noahll597@gmail.com>
2022-03-19 15:49:54 -0400

    variables and mixins + header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./styles/App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./styles/App.scss";
-import "./styles/partials/_breakpoints.scss";
+import "./styles/partials/breakpoints.scss";
 import Header from "./components/Header";
 
 export default class App extends Component {
