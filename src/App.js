5631f807365c13de4acac3b4c2affb59451a463b
NoahLaroi <noahll597@gmail.com>
2022-03-19 13:19:42 -0400

    finished header component mobile styling--mostly

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-<<<<<<< HEAD
-import "./App.css";
+import "./styles/App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Component } from "react";
-
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Header />
-          <Route exact path="/" component={WareHouse} />
-=======
 import logo from "./logo.svg";
 import "./styles/App.scss";
 import "./styles/partials/_breakpoints.scss";
 import Header from "./components/Header";
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
-}
->>>>>>> 3b85e06f6436576006dd70a03db31bd46b9138f4
 
-          <Route
-            path="/:warehouseid/inventory"
-            render={(RouterProps) => {
-              return <Inventory match={RouterProps} />;
-            }}
-          />
-        </Switch>
-        <Footer />
-      </Router>
-    );
+export default class App extends Component {
+  render() {
+    return <Header />;
   }
 }
