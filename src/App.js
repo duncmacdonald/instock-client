9db6ab50e64398b2aa9b557835a2989a81d03d14
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-19 16:06:54 -0400

    Fixed the issue with app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import "./styles/App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Component } from "react";
-<<<<<<< HEAD
 
 export default class App extends Component {
   render() {
@@ -10,23 +8,16 @@ export default class App extends Component {
         <Switch>
           <Header />
           <Route exact path="/" component={WareHouse} />
-=======
-import "./styles/App.scss";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Component } from "react";
-=======
->>>>>>> 5631f807365c13de4acac3b4c2affb59451a463b
-import logo from "./logo.svg";
-import "./styles/App.scss";
-import "./styles/partials/_breakpoints.scss";
-import Header from "./components/Header";
-<<<<<<< HEAD
->>>>>>> 5631f807365c13de4acac3b4c2affb59451a463b
-=======
->>>>>>> 5631f807365c13de4acac3b4c2affb59451a463b
 
-export default class App extends Component {
-  render() {
-    return <Header />;
+          <Route
+            path="/:warehouseid/inventory"
+            render={(RouterProps) => {
+              return <Inventory match={RouterProps} />;
+            }}
+          />
+        </Switch>
+        <Footer />
+      </Router>
+    );
   }
 }
