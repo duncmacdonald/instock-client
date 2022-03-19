141fb1806b6f0e16dd35730a4e08fb70c2b62340
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-19 17:11:45 -0400

    added the hover details for button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Component } from "react";
+import DeleteWarehouse from "./components/Component/DeleteWarehouse";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Header />
-          <Route exact path="/" component={WareHouse} />
+          <Route exact path="/" component={DeleteWarehouse} />
 
           <Route
             path="/:warehouseid/inventory"
             render={(RouterProps) => {
-              return <Inventory match={RouterProps} />;
+              return <DeleteWarehouse match={RouterProps} />;
             }}
           />
         </Switch>
-        <Footer />
       </Router>
     );
   }
 }
+
+/*
+
+      
+      */
