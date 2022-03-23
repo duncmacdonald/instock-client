6d4c49bba0738a6207b50795c42b0cd09f0f4e9c
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-23 07:49:10 -0400

    added the states and changed it to a class component in warehouses.js

--- src/components/DeleteWarehouse/warehouseComponents/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/warehouseComponents/DeleteWarehouse.js
@@ -8,11 +8,12 @@ export class DeleteWarehouse extends Component {
         <div className="delete-warehouse">
           <p className="delete-warehouse__close-button-top">X</p>
           <h1 className="delete-warehouse__heading">
-            Delete Washington warehouse
+            Delete {this.props.warehouseInfo[1].name} warehouse
           </h1>
           <p className="delete-warehouse__confirmation-message">
-            Please confirm that you’d like to delete the Washington from the
-            list of warehouses. You won’t be able to undo this action.
+            Please confirm that you’d like to delete the{" "}
+            {this.props.warehouseInfo[1].name} from the list of warehouses. You
+            won’t be able to undo this action.
           </p>
 
           <div className="delete-warehouse__button-container">
--- src/components/WarehousesTable/WarehousesTable.js
+++ src/components/WarehousesTable/WarehousesTable.js
@@ -1,22 +1,18 @@
 import TableTitles from "../TableTitles/TableTitles";
 import TableRowWarehouse from "../TableRowWarehouse/TableRowWarehouse";
 import "./WarehousesTable.css";
-import "../../index.css"
+import "../../index.css";
 
+export default function WarehousesTable({ titles, contentArray }) {
+  //Rows
+  const rowsJSX = contentArray.map((row) => {
+    return <TableRowWarehouse warehouse={row} />;
+  });
 
-export default function WarehousesTable({ titles, contentArray}){
-
-    //Rows
-    const rowsJSX = contentArray.map(row => {
-        return(
-            <TableRowWarehouse warehouse={row} />
-        )
-    })
-
-    return(
-        <>
-            <TableTitles titles={titles}/>
-            {rowsJSX}
-        </>
-    )
+  return (
+    <>
+      <TableTitles titles={titles} />
+      {rowsJSX}
+    </>
+  );
 }
