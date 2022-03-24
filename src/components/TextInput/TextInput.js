5ebf3a42cefd3f1386cce4304c4e2354edaf31a9
Duncan Macdonald <dunc.macdonald@gmail.com>
2022-03-24 14:36:48 -0700

    Added Label

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -6,19 +6,21 @@ import exclaim from "../../assets/Icons/error-24px.svg";
 //   value = state from the form you are building
 //   name = match the name from your state
 //   placeholder = the text when empty
+//   label = the lable
 //   listener = listener that listen an event with name and value -> save to state. Also set error to false.
 //   error = true/false flag
 
-export default function TextInput({ name, value, placeholder, listener, error }) {
-  let inputClass = error ? "TextInput error" : "TextInput";
+export default function TextInput({ name, value, label, placeholder, listener, error }) {
+  let inputClass = error ? "error" : "";
 
-  let errorNotification = error ? "ErrorNotification" : "ErrorNotification hidden";
+  let errorNotification = error ? "" : "hidden";
 
   return (
-    <>
+    <div className="TextInput">
+      <label for={name}>{label}</label>
       <input
-        className={inputClass}
         type="text"
+        id={name}
         name = {name}
         placeholder={placeholder}
         value={value}
@@ -26,10 +28,10 @@ export default function TextInput({ name, value, placeholder, listener, error })
           listener(event);
         }}
       ></input>
-      <div className={errorNotification}>
+      <div className="ErrorNotification">
         <img src={exclaim} alt="error"></img>
         <span>This field is required</span>
       </div>
-    </>
+    </div>
   );
 }
