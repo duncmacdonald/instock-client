223a9e0bdd5cb2c2f92fc739d4fe8d6418714cbe
Duncan Macdonald <dunc.macdonald@gmail.com>
2022-03-24 18:55:45 -0700

    Validation added. Todo: add axios call

--- src/pages/AddWarehouse/AddWarehouse.js
+++ src/pages/AddWarehouse/AddWarehouse.js
@@ -3,12 +3,77 @@ import axios from "axios";
 import Button from "../../components/Button/Button";
 import TextInput from "../../components/TextInput/TextInput";
 import back from "../../assets/Icons/arrow_back-24px.svg";
+import email from "email-validator";
 import "./AddWarehouse.css";
 import "../../index.css";
 const URL = "http://localhost:8080/warehouse/";
 
 export default class AddWarehouse extends React.Component {
-  state = {};
+  state = {
+    name: "",
+    address: "",
+    city: "",
+    country: "",
+    contactName: "",
+    contactPosition: "",
+    contactPhone: "",
+    contactEmail: "",
+    errors: {
+      name: false,
+      address: false,
+      city: false,
+      country: false,
+      contactName: false,
+      contactPosition: false,
+      contactPhone:false,
+      contactEmail:false,
+    },
+  };
+
+  formListener = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addWarehouse = () => {
+      if(this.formValidator()){
+          console.log("axios submit then link to main page");
+      }
+  }
+
+
+  formValidator = () => {
+      const keys = Object.keys(this.state);
+      const data = Object.values(this.state);
+      const errors = data.pop();
+      let rtrnValue = true;
+
+      //Check that some data exists
+      data.forEach((field,i) =>{
+          if(field.length < 3){
+              errors[keys[i]]=true;
+              rtrnValue = false
+          } else {
+            errors[keys[i]]=false;
+          }
+      })
+
+      if(!(email.validate(this.state.contactEmail))){
+          errors.contactEmail = true;
+          rtrnValue = false;
+      }
+
+      if(!(this.state.contactPhone.length === 17 && this.state.contactPhone.substring(0,4) === "+1 (" && this.state.contactPhone.substring(7,9) === ") " &&this.state.contactPhone.charAt(12) === "-")) {
+        setTimeout(() => alert("Phone number format must be: +1 (###) ###-####"), 1);
+          errors.contactPhone = true;
+          rtrnValue = false;
+      }
+
+      this.setState({errors: errors});
+      
+      
+      return rtrnValue;
+  }
+
 
   render() {
     return (
@@ -24,25 +89,33 @@ export default class AddWarehouse extends React.Component {
               label="Warehouse Name"
               placeholder="Warehouse Name"
               name="name"
-              error={false}
+              value={this.state.name}
+              listener={this.formListener}
+              error={this.state.errors.name}
             />
             <TextInput
               label="Street Address"
               placeholder="Street Address"
               name="address"
-              error={false}
+              value={this.state.address}
+              listener={this.formListener}
+              error={this.state.errors.address}
             />
             <TextInput
               label="City"
               placeholder="City"
               name="city"
-              error={false}
+              value={this.state.city}
+              listener={this.formListener}
+              error={this.state.errors.city}
             />
             <TextInput
               label="Country"
               placeholder="Country"
               name="country"
-              error={false}
+              value={this.state.country}
+              listener={this.formListener}
+              error={this.state.errors.country}
             />
           </section>
           <section>
@@ -50,31 +123,39 @@ export default class AddWarehouse extends React.Component {
             <TextInput
               label="Contact Name"
               placeholder="Contact Name"
-              name="contact.name"
-              error={false}
+              name="contactName"
+              value={this.state.contactName}
+              listener={this.formListener}
+              error={this.state.errors.contactName}
             />
             <TextInput
               label="Position"
               placeholder="Position"
-              name="contact.position"
-              error={false}
+              name="contactPosition"
+              value={this.state.contactPosition}
+              listener={this.formListener}
+              error={this.state.errors.contactPosition}
             />
             <TextInput
               label="Phone Number"
               placeholder="Phone Number"
-              name="addrcityess"
-              error={false}
+              name="contactPhone"
+              value={this.state.contactPhone}
+              listener={this.formListener}
+              error={this.state.errors.contactPhone}
             />
             <TextInput
               label="Email"
               placeholder="Email"
-              name="contact.email"
-              error={false}
+              name="contactEmail"
+              value={this.state.contactEmail}
+              listener={this.formListener}
+              error={this.state.errors.contactEmail}
             />
           </section>
           <div className="ButtonFlex">
             <Button color="white" text="Cancel" />
-            <Button color="blue" text="+ Add Warehouse" />
+            <Button color="blue" text="+ Add Warehouse" action={this.addWarehouse}/>
           </div>
         </form>
       </section>
